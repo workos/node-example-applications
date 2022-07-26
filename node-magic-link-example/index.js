89e5123fe32dc7e2543be6c07d8bb1040ec9c9d1
Edita Ignot <edignot@Editas-MacBook-Pro.local>
2022-07-26 15:16:22 -0600

    Refactored code to use ESM, updated UI

--- node-magic-link-example/index.js
+++ node-magic-link-example/index.js
@@ -1,81 +1,20 @@
-const express = require("express");
-const path = require("path");
-const WorkOS = require('@workos-inc/node').default;
-const bodyParser = require('body-parser');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-require('dotenv').config()
-process.on('unhandledRejection', (reason) => { throw reason });
+import express from 'express'
+import 'dotenv/config'
+import router from './routes/index.js'
+import morgan from 'morgan'
 
-// App Variables
-const app = express();
-const port = "8000";
-const workos = new WorkOS(process.env.WORKOS_API_KEY);
-const clientID = process.env.WORKOS_CLIENT_ID;
+const app = express()
 
-// App Configuration
-app.set("views", path.join(__dirname, "views"));
-app.engine('html', require('ejs').renderFile);
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.urlencoded({ extended: true }));
+const port = process.env.PORT || 8000
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname)));
+app.use('/public', express.static('public'))
 
+app.use(express.urlencoded({ extended: false }))
 
+app.use(morgan('dev'))
 
-// Route Definitions
-
- app.get("/", async (req, res) => {
-  res.render("index.ejs", {
-    title: "Home",
-  });
-});
-
-app.post('/passwordless-auth', async (req, res) => {
-  const email = req.body.email
-  const session_options = {
-    email,
-    type: 'MagicLink'
-  };
-  // API call to generate a new passwordless session.
-  const session = await workos.passwordless.createSession(session_options);
-  console.log(session)
-
-  // API call to send email, passing in session ID generate above.
-  await workos.passwordless.sendSession(session.id);
-
-  res.render("confirmation.ejs", {
-    email: session.email,
-    link: session.link,
-  })
-});
-
-app.get("/success", async (req, res) => {
-  // Retrieve auth code from query parameter
-  const code = req.query.code;
-  
-  // Exchange auth code for profile
-  const profile = await workos.sso.getProfileAndToken({
-    code,
-    clientID,
-  });
-
-  let p_profile = JSON.stringify(profile);
-  
-  res.render("login_successful.ejs", {
-    profile: p_profile,
-  });
-});
-
-
-
-// Server Activation
+app.use('/', router)
 
 app.listen(port, () => {
-  console.log(`Listening to requests on http://localhost:${port}`);
-});
+    console.log(`⚡️ [server]: Server is running at https://localhost:${port}`)
+})
---
+++ node-magic-link-example/routes/index.js
@@ -0,0 +1,47 @@
+import express from 'express'
+const router = express.Router()
+import { WorkOS } from '@workos-inc/node'
+
+const workos = new WorkOS(process.env.WORKOS_API_KEY)
+
+const clientID =
+    process.env.WORKOS_CLIENT_ID !== undefined
+        ? process.env.WORKOS_CLIENT_ID
+        : ''
+
+router.get('/', async (req, res) => {
+    res.render('index.ejs', {
+        title: 'Home',
+    })
+})
+
+router.post('/passwordless-auth', async (req, res) => {
+    const email = req.body.email
+
+    const session = await workos.passwordless.createSession({
+        email: email,
+        type: 'MagicLink',
+    })
+
+    await workos.passwordless.sendSession(session.id)
+
+    res.render('confirmation.ejs', {
+        email: session.email,
+        link: session.link,
+    })
+})
+
+router.get('/success', async (req, res) => {
+    const code = req.query.code
+
+    const profile = await workos.sso.getProfileAndToken({
+        code,
+        clientID,
+    })
+
+    res.render('login_successful.ejs', {
+        profile: JSON.stringify(profile, null, 4),
+    })
+})
+
+export default router
