333f544f4141c33658e5278eda618ffd76c872ab
Edita Ignot <57964291+edignot@users.noreply.github.com>
2022-08-04 16:33:18 -0600

    Feature/sup 541 node update mfa example app (#30)
    * CommonJS changed to ES6
    
    * UI and styles updated
    
    * Logo replaced
    
    * Cleanup
    
    Co-authored-by: Edita Ignot <edignot@Editas-MacBook-Pro.local>

---
+++ node-mfa-example/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import 'dotenv/config'
+import router from './routes/index.js'
+import morgan from 'morgan'
+
+const app = express()
+
+const port = process.env.PORT || 8000
+
+app.use('/public', express.static('public'))
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.json())
+
+app.use(morgan('dev'))
+
+app.use('/', router)
+
+app.listen(port, () => {
+    console.log(`⚡️ [server]: Server is running at https://localhost:${port}`)
+})
--- node-mfa-example/routes/index.js
+++ node-mfa-example/routes/index.js
@@ -1,95 +1,126 @@
-var express = require('express');
-var router = express.Router();
-var app = express();
-var session = require('express-session');
+import express from 'express'
+import session from 'express-session'
+import { WorkOS } from '@workos-inc/node'
 
-app.use(session({ 
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true } 
-}));
+const app = express()
+const router = express.Router()
 
-const WorkOS = require('@workos-inc/node').default;
-const workos = new WorkOS(process.env.WORKOS_API_KEY);
-factors = session.factors = [];
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: true },
+    })
+)
 
-router.get('/', function(req, res) {
-  res.render('index.ejs', { title: 'Home' });
-});
+const workos = new WorkOS(process.env.WORKOS_API_KEY)
+session.factors = []
 
-router.post('/enroll_factor', async function(req, res) {
-  if (req.body.type === "sms") {
-    factor_type = req.body.type;
-    phone_number = '+1' + req.body.phone_number;
+router.get('/', function (req, res) {
+    res.render('index.ejs', { title: 'Home', factors: session.factors })
+})
 
-    new_factor = await workos.mfa.enrollFactor({
-      type: factor_type,
-      phoneNumber: phone_number,
-    });
-  } else if (req.body.type === "totp") {
-    factor_type = req.body.type;
-    issuer = req.body.totp_issuer;
-    user = req.body.totp_user;
+router.get('/enroll_factor', (req, res) => {
+    res.render('enroll_factor.ejs')
+})
 
-    new_factor = await workos.mfa.enrollFactor({
-      type: factor_type,
-      issuer: issuer,
-      user: user,
-    });
-  }
+router.post('/enroll_new_factor', async function (req, res) {
+    let factor_type
+    let new_factor
 
-  factors.push(new_factor);
-  res.render('index.ejs', { factors: factors });
-});
+    if (req.body.type === 'sms') {
+        factor_type = req.body.type
+        const phone_number = '+1' + req.body.phone_number
+
+        new_factor = await workos.mfa.enrollFactor({
+            type: factor_type,
+            phoneNumber: phone_number,
+        })
+    } else if (req.body.type === 'totp') {
+        factor_type = req.body.type
+        const issuer = req.body.totp_issuer
+        const user = req.body.totp_user
+
+        new_factor = await workos.mfa.enrollFactor({
+            type: factor_type,
+            issuer: issuer,
+            user: user,
+        })
+    }
+
+    session.factors.push(new_factor)
+    res.render('index.ejs', { factors: session.factors })
+})
 
 router.get('/factor_detail/:id', async (req, res) => {
-   const factor = await factors.filter((factor) => {
-     return factor.id == req.params.id
-   })[0]
+    const factor = await session.factors.filter((factor) => {
+        return factor.id == req.params.id
+    })[0]
 
-  session.current_factor = factor;
-  res.render('factor_detail.ejs', { title: 'Factor Detail', factor: factor });
-});
+    session.current_factor = factor
+    res.render('factor_detail.ejs', { title: 'Factor Detail', factor: factor })
+})
 
 router.post('/challenge_factor', async (req, res) => {
-  if (session.current_factor.type === "sms") {
-    message = req.body.sms_message;
-    session.sms_message = message;
+    let message
 
-    const challenge = await workos.mfa.challengeFactor({
-      authenticationFactorId: session.current_factor.id,
-      smsTemplate: message,
-    });
-    session.challenge_id = challenge.id
-  }
+    if (session.current_factor.type === 'sms') {
+        message = req.body.sms_message
+        session.sms_message = message
 
-  if (session.current_factor.type === "totp") {
-    message = req.body.sms_message;
-    session.sms_message = message;
+        const challenge = await workos.mfa.challengeFactor({
+            authenticationFactorId: session.current_factor.id,
+            smsTemplate: message,
+        })
 
-    const challenge = await workos.mfa.challengeFactor({
-      authenticationFactorId: session.current_factor.id,
-    });
-    console.log(JSON.stringify(challenge));
-    session.challenge_id = challenge.id
-  }
+        session.challenge_id = challenge.id
+    }
 
-  res.render('challenge_factor.ejs', { title: 'Challenge Factor' });
-});
+    if (session.current_factor.type === 'totp') {
+        message = req.body.sms_message
+        session.sms_message = message
+
+        const challenge = await workos.mfa.challengeFactor({
+            authenticationFactorId: session.current_factor.id,
+        })
+
+        session.challenge_id = challenge.id
+    }
+
+    res.render('challenge_factor.ejs', { title: 'Challenge Factor' })
+})
 
 router.post('/verify_factor', async (req, res) => {
-  code = req.body.code;
-  challenge_id = session.challenge_id;
-  verify_factor = await workos.mfa.verifyFactor({
-    authenticationChallengeId: challenge_id,
-    code: code,
-  });
-  res.render('challenge_success.ejs', { title: 'Challenge Success', verify_factor: verify_factor });
-});
+    const buildCode = (codeItems) => {
+        let code = []
+        for (const item in codeItems) {
+            code.push(codeItems[item])
+        }
+        return code.join('')
+    }
+
+    const code = buildCode(req.body)
+    const challenge_id = session.challenge_id
+
+    const verify_factor = await workos.mfa.verifyFactor({
+        authenticationChallengeId: challenge_id,
+        code: code,
+    })
+
+    res.render('challenge_success.ejs', {
+        title: 'Challenge Success',
+        verify_factor: verify_factor,
+    })
+})
 
 router.get('/challenge_success', async (req, res) => {
-  res.render('challenge_success.ejs', { title: 'Challenge Success' });
-});
+    res.render('challenge_success.ejs', { title: 'Challenge Success' })
+})
 
-module.exports = router;
+router.get('/clear_session', (req, res) => {
+    session.factors = []
+    res.redirect('/')
+})
+
+export default router
