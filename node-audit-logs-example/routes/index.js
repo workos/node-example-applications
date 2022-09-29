4f7c8501e188b5ba78a178792689db9bd9562942
Edita Ignot <edignot@gmail.com>
2022-09-29 12:48:48 -0600

    post audit events to workos

--- node-audit-logs-example/routes/index.js
+++ node-audit-logs-example/routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import session from 'express-session'
 import { WorkOS } from '@workos-inc/node'
-import { user_signed_in, user_logged_out, user_organization_set, user_organization_deleted, user_connection_deleted } from '../audit_log_events.js'
+import { user_signed_in, user_logged_out, user_organization_deleted, user_connection_deleted } from '../audit_log_events.js'
 
 const app = express()
 const router = express.Router()
@@ -36,14 +36,33 @@ router.post('/set_org', async (req, res) => {
 })
 
 router.post('/send_event', async (req, res) => {
-    
-    console.log('Test', req.body)
-    // res.render('login.ejs')
+    const eventId = req.body.eventId
+    let event;
+
+    switch(eventId) {
+        case 'user_signed_in':
+            event = user_signed_in
+            break
+        case 'user_logged_out':
+            event = user_logged_out
+            break
+        case 'user_organization_deleted':
+            event = user_organization_deleted
+            break
+        case 'user_connection_deleted':
+            event = user_connection_deleted
+            break
+    }
+
+    await workos.auditLogs.createEvent(
+        session.orgId || 'org_01G2TKRPR28XB702EF71EA8BY6',
+        event
+    )
+
+    console.log(event)
 })
 
 export default router
-
-// org_01G2TKRPR28XB702EF71EA8BY6
 
 
 
