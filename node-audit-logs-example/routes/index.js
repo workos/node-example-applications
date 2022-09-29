9a455cc53304fb79d77136e29d84c5f670efc788
Edita Ignot <edignot@gmail.com>
2022-09-28 20:52:16 -0600

    send events page created

--- node-audit-logs-example/routes/index.js
+++ node-audit-logs-example/routes/index.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import session from 'express-session'
 import { WorkOS } from '@workos-inc/node'
+import { user_signed_in, user_logged_out, user_organization_set, user_organization_deleted, user_connection_deleted } from '../audit_log_events.js'
 
 const app = express()
 const router = express.Router()
+
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: true },
+    })
+)
 
 const workos = new WorkOS(process.env.WORKOS_API_KEY)
 
@@ -10,8 +21,12 @@ router.get('/', function (req, res) {
     res.render('login.ejs')
 })
 
-router.get('/enroll_factor', (req, res) => {
-    res.render('enroll_factor.ejs')
+router.post('/set_org', (req, res) => {
+    session.org = req.body.org
+//get customer by org id
+    res.render('send_events.ejs', {
+        org: session.org
+    })
 })
 
 export default router
