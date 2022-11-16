e9012dbec2daa6c80e3659757939323720b99592
Rob Frampton <rob@workos.com>
2022-11-16 10:12:35 -0500

    Update audit log target types

--- node-audit-logs-example/audit_log_events.js
+++ node-audit-logs-example/audit_log_events.js
@@ -1,96 +1,102 @@
 const user_signed_in = {
-    "action": "user.signed_in",
-    "occurred_at": new Date().toISOString(),
-    "actor": {
-        "type": "user",
-        "id": "user_01GBNJC3MX9ZZJW1FSTF4C5938",
+  action: "user.signed_in",
+  occurred_at: new Date().toISOString(),
+  actor: {
+    type: "user",
+    id: "user_01GBNJC3MX9ZZJW1FSTF4C5938",
+  },
+  targets: [
+    {
+      type: "team",
+      id: "team_01GBNJD4MKHVKJGEWK42JNMBGS",
     },
-    "targets": [
-        {
-            "type": "team",
-            "id": "team_01GBNJD4MKHVKJGEWK42JNMBGS",
-        },
-    ],
-    "context": {
-        "location": "123.123.123.123",
-        "user_agent": "Chrome/104.0.0.0",
-    },
-}
+  ],
+  context: {
+    location: "123.123.123.123",
+    user_agent: "Chrome/104.0.0.0",
+  },
+};
 
 const user_logged_out = {
-    "action": "user.logged_out",
-    "occurred_at": new Date().toISOString(),
-    "actor": {
-        "type": "user",
-        "id": "user_01GBNJC3MX9ZZJW1FSTF4C5938",
+  action: "user.logged_out",
+  occurred_at: new Date().toISOString(),
+  actor: {
+    type: "user",
+    id: "user_01GBNJC3MX9ZZJW1FSTF4C5938",
+  },
+  targets: [
+    {
+      type: "team",
+      id: "team_01GBNJD4MKHVKJGEWK42JNMBGS",
     },
-    "targets": [
-        {
-            "type": "team",
-            "id": "team_01GBNJD4MKHVKJGEWK42JNMBGS",
-        },
-    ],
-    "context": {
-        "location": "123.123.123.123",
-        "user_agent": "Chrome/104.0.0.0",
-    },
-}
+  ],
+  context: {
+    location: "123.123.123.123",
+    user_agent: "Chrome/104.0.0.0",
+  },
+};
 
 const user_organization_set = {
-    "action": "user.organization_set",
-    "occurred_at": new Date().toISOString(),
-    "actor": {
-        "type": "user",
-        "id": "user_01GBNJC3MX9ZZJW1FSTF4C5938",
+  action: "user.organization_set",
+  occurred_at: new Date().toISOString(),
+  actor: {
+    type: "user",
+    id: "user_01GBNJC3MX9ZZJW1FSTF4C5938",
+  },
+  targets: [
+    {
+      type: "organization",
+      id: "org_01GBNJD4MKHVKJGEWK42JNMBGS",
     },
-    "targets": [
-        {
-            "type": "team",
-            "id": "team_01GBNJD4MKHVKJGEWK42JNMBGS",
-        },
-    ],
-    "context": {
-        "location": "123.123.123.123",
-        "user_agent": "Chrome/104.0.0.0",
-    },
-}
+  ],
+  context: {
+    location: "123.123.123.123",
+    user_agent: "Chrome/104.0.0.0",
+  },
+};
 
 const user_organization_deleted = {
-    "action": "user.organization_deleted",
-    "occurred_at": new Date().toISOString(),
-    "actor": {
-        "type": "user",
-        "id": "user_01GBNJC3MX9ZZJW1FSTF4C5938",
+  action: "user.organization_deleted",
+  occurred_at: new Date().toISOString(),
+  actor: {
+    type: "user",
+    id: "user_01GBNJC3MX9ZZJW1FSTF4C5938",
+  },
+  targets: [
+    {
+      type: "organization",
+      id: "org_01GBNJD4MKHVKJGEWK42JNMBGS",
     },
-    "targets": [
-        {
-            "type": "team",
-            "id": "team_01GBNJD4MKHVKJGEWK42JNMBGS",
-        },
-    ],
-    "context": {
-        "location": "123.123.123.123",
-        "user_agent": "Chrome/104.0.0.0",
-    },
-}
+  ],
+  context: {
+    location: "123.123.123.123",
+    user_agent: "Chrome/104.0.0.0",
+  },
+};
 
 const user_connection_deleted = {
-    "action": "user.connection_deleted",
-    "occurred_at": new Date().toISOString(),
-    "actor": {
-        "type": "user",
-        "id": "user_01GBNJC3MX9ZZJW1FSTF4C5938",
+  action: "user.connection_deleted",
+  occurred_at: new Date().toISOString(),
+  actor: {
+    type: "user",
+    id: "user_01GBNJC3MX9ZZJW1FSTF4C5938",
+  },
+  targets: [
+    {
+      type: "connection",
+      id: "connection_01GBNJD4MKHVKJGEWK42JNMBGS",
     },
-    "targets": [
-        {
-            "type": "team",
-            "id": "team_01GBNJD4MKHVKJGEWK42JNMBGS",
-        },
-    ],
-    "context": {
-        "location": "123.123.123.123",
-        "user_agent": "Chrome/104.0.0.0",
-    },
-}
+  ],
+  context: {
+    location: "123.123.123.123",
+    user_agent: "Chrome/104.0.0.0",
+  },
+};
 
-export { user_signed_in, user_logged_out, user_organization_set, user_organization_deleted, user_connection_deleted }
+export {
+  user_signed_in,
+  user_logged_out,
+  user_organization_set,
+  user_organization_deleted,
+  user_connection_deleted,
+};
