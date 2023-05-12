816ab8dfb836bdc966bc8064384625636ece5712
Edita Ignot <edignot@gmail.com>
2023-05-12 09:19:10 -0600

    admin ortal app format

--- node-admin-portal-example/routes/index.js
+++ node-admin-portal-example/routes/index.js
@@ -1,19 +1,19 @@
-import express from "express"
-import { WorkOS } from "@workos-inc/node"
+import express from 'express'
+import { WorkOS } from '@workos-inc/node'
 
 const workos = new WorkOS(process.env.WORKOS_API_KEY)
 
 const router = express.Router()
 
-router.get("/", async (req, res) => {
-    res.render("index.ejs", {
-        title: "Home",
+router.get('/', async (req, res) => {
+    res.render('index.ejs', {
+        title: 'Home',
     })
 })
 
-router.post("/provision_enterprise", async (req, res) => {
+router.post('/provision_enterprise', async (req, res) => {
     const orgName = req.body.org
-    const domains = req.body.domain.split(" ")
+    const domains = req.body.domain.split(' ')
 
     let orgs = await workos.organizations.listOrganizations({
         domains: domains,
@@ -28,10 +28,10 @@ router.post("/provision_enterprise", async (req, res) => {
         })
     }
 
-    res.render("logged_in.ejs")
+    res.render('logged_in.ejs')
 })
 
-router.get("/launch_admin_portal", async (req, res) => {
+router.get('/launch_admin_portal', async (req, res) => {
     const intent = req.query.intent
 
     const { link } = await workos.portal.generateLink({
